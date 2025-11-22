290d9aedd883d13bc327be11964bf51ad43c89bd
x-neon-nexus-o <td240430@famt.ac.in>
2025-11-22 13:03:23 +0530

    feat: Add GitHub Pages deployment configuration and scripts.

--- vite.config.js
+++ vite.config.js
@@ -4,4 +4,5 @@ import react from '@vitejs/plugin-react'
 // https://vite.dev/config/
 export default defineConfig({
   plugins: [react()],
+  base: '/Swad-N-Sip/',  // Leading and trailing slashes are important!
 })
